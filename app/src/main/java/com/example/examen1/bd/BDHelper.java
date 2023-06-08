aac19e0f0f9912bc9961f33117554c8d761adfa4
Michaelbadango <126106195+Michaelbadango@users.noreply.github.com>
2023-06-08 15:52:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/examen1/bd/BDHelper.java
@@ -0,0 +1,35 @@
+package com.example.examen1.bd;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class BDHelper extends SQLiteOpenHelper {
+    public BDHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE t_funcionario"+"(" +
+                "usu_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "f_nombre tetx NOT NULL," +
+                "f_cargo text NOT NULL," +
+                "f_area text NOT NULL," +
+                "f_estado text NOT NULL," +
+                "f_hijos text NOT NULL," +
+                "f_sueldo text NOT NULL," +
+                "f_subsidio NOT NULL," +
+                "f_atrasos text NOT NULL," +
+                "f_horas text NOT NULL," +
+                "f_sueldoT text NOT NULL)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE t_funcionario");
+        onCreate(db);
+    }
+}
