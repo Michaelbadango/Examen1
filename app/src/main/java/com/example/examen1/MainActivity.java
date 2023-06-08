9997983c1273d08bd610690d226d915ca02909c7
Michaelbadango <126106195+Michaelbadango@users.noreply.github.com>
2023-06-08 16:38:03 -0500

    correcion de diseño

--- app/src/main/java/com/example/examen1/MainActivity.java
+++ app/src/main/java/com/example/examen1/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.examen1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.examen1.bd.BDHelper;
 
 public class MainActivity extends AppCompatActivity {
     EditText txt_nombre, txt_cargo, txt_area, txt_estado, txt_hijos, txt_sueldo, txt_subsidio,
@@ -27,6 +32,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void registrar(View view){
-        
+        BDHelper admin = new BDHelper(this,"Registro.db",null,1);
+        SQLiteDatabase bd = admin.getWritableDatabase();
+        String nombre = txt_nombre.getText().toString();
+        String cargo = txt_cargo.getText().toString();
+        String area = txt_area.getText().toString();
+        String estado = txt_estado.getText().toString();
+        int hijos = Integer.parseInt(txt_hijos.getText().toString());
+        String atrasos = txt_atrasos.getText().toString();
+        String horas = txt_horas.getText().toString();
+
+        if(!nombre.isEmpty() && !cargo.isEmpty() && area.isEmpty() && !estado.isEmpty() && !atrasos.isEmpty() && !horas.isEmpty()){
+            ContentValues registro = new ContentValues();
+            registro.put("f_nombre",nombre);
+            registro.put("f_cargo",cargo);
+            registro.put("f_area",area);
+            registro.put("f_estado",estado);
+            registro.put("f_hijos",hijos);
+
+            // Calcular el sueldo base en función del cargo
+            double sueldoBase = 0;
+            if (cargo.equalsIgnoreCase("docente")) {
+                sueldoBase = 1000;
+            } else if (cargo.equalsIgnoreCase("funcionario")) {
+                sueldoBase = 880;
+            }
+            registro.put("f_sueldo",sueldoBase);
+
+            // Calcular el subsidio en función del número de hijos
+            double subsidio = hijos * 50;
+            registro.put("f_subsidio",subsidio);
+
+
+            registro.put("f_atrasos",atrasos);
+            registro.put("f_horas",horas);
+
+            bd.insert("t_funcionario", null,registro);
+            Toast.makeText(this, "REGISTRO EXITOSO", Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
